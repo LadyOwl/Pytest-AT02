0b331e380c06957ab2ff36fcc70168418b00f150
Юлия (Джулия) <sixquestions666@gmail.com>
2026-05-03 07:30:18 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def count_vowels(text: str) -> int:
+    
+    vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"
+    count = 0
+    for char in text:
+        if char in vowels:
+            count += 1
+    return count
